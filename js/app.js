30818b17216df26f68feca0daca808b810af5142
zohirjon <example@gmail.com>
2024-01-07 20:34:41 +0900

    first commit

---
+++ js/app.js
@@ -0,0 +1,27 @@
+"use strict"
+
+
+// const currrenPlace = prompt('Are you here ?', 'zohirjon');
+
+// console.log(currrenPlace);
+
+
+// const myLoveColor= [];
+// myLoveColor[0]= prompt('Whats your like color ? #1' ,'' );
+// myLoveColor[1]= prompt('Whats your like color ? #2' ,'' );
+// myLoveColor[2]= prompt('Whats your like color ? #3' ,'' );
+
+// console.log( myLoveColor);
+
+// const user = 'zohirjon';
+// const fname= 'alimardonov';
+
+// console.log(`Meni ismim ${user}, familiyam esa ${fname}`);
+
+
+console.log(5%2,10%4, 5/1 === '5');
+
+const isAge = true;
+const isClose=false;
+
+console.log(isAge || isClose);
